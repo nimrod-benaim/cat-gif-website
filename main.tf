5e78e5361dca310e4e06659095990aa612176867
Nimrod Benaim <Nimrodbenaim@gmail.com>
2025-01-26 10:23:17 +0200

    fix for cicd

--- main.tf
+++ main.tf
@@ -66,8 +66,8 @@ resource "aws_instance" "docker_host" {
     sudo yum install -y libxcrypt-compat
     sudo service docker start
     sudo usermod -a -G docker ec2-user
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
     echo "Docker and Docker Compose installed."
 
     # Clone your project repository
