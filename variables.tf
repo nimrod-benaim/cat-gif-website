73413a41fbb432ff180e985a15d0df6d148c864f
Nimrod Benaim <Nimrodbenaim@gmail.com>
2025-01-27 12:27:27 +0200

    added fix to varybles

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "DATABASE_HOST" {}
-variable "DATABASE_PORT" {}
-variable "DATABASE_USER" {}
-variable "DATABASE_PASSWORD" {}
-variable "DATABASE_NAME" {}
-variable "MYSQL_ROOT_PASSWORD" {}
-variable "KEY_PAIR_NAME" {}
-variable "IAM_USER_NAME" {}
+variable "database_host" {}
+variable "database_port" {}
+variable "database_user" {}
+variable "database_password" {}
+variable "database_name" {}
+variable "mysql_root_password" {}
+variable "key_pair_name" {}
+variable "iam_user_name" {}
