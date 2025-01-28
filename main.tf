8e801027d0df39e6686dba0ab3b42743453f6830
Nimrod Benaim <Nimrodbenaim@gmail.com>
2025-01-28 08:40:56 +0200

    what omri said

--- main.tf
+++ main.tf
@@ -2,6 +2,54 @@ provider "aws" {
   region = "us-east-1" # Adjust to your desired AWS region
 }
 
+
+variable "database_host" {
+  description = "The host of the database"
+  type        = string
+}
+
+variable "database_port" {
+  description = "The port of the database"
+  type        = string
+}
+
+variable "database_user" {
+  description = "The database username"
+  type        = string
+}
+
+variable "database_password" {
+  description = "The database password"
+  type        = string
+}
+
+variable "database_name" {
+  description = "The name of the database"
+  type        = string
+}
+
+variable "mysql_root_password" {
+  description = "The root password for MySQL"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "The AWS key pair name"
+  type        = string
+}
+
+variable "iam_user_name" {
+  description = "The IAM user name"
+  type        = string
+}
+
+variable "port" {
+  description = "The port number the application will use."
+  type        = number
+  default     = 5000 # Default to Flask's standard port; modify if needed
+}
+
+
 # Check if the security group already exists
 data "aws_security_group" "existing" {
   filter {
