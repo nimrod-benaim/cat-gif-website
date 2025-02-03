a3e30a8e55ee7dcbfcd3eadb75a7a301d7e7a665
Nimrod Benaim <Nimrodbenaim@gmail.com>
2025-02-03 09:02:37 +0200

    25 k8s + tera test

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable "gcp_credentials_path" {
 
 resource "google_container_cluster" "primary" {
   name     = "cat-gif-cluster"
-  location = "us-central1"
+  location = "us-central1-a"
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -23,7 +23,7 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "node-pool"
-  location   = "us-central1"
+  location   = "us-central1-a"
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
